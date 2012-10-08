7715aeb4ef375847d6462dda065bfc03962cd238
Jamie Kimmell <kimmellj@gmail.com>
2012-10-08 18:56:43 -0400

    Finished up the GetMany for Clients

--- private/main.go
+++ private/main.go
@@ -1,21 +1,96 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"code.google.com/p/goweb/goweb"
+	"fmt"
+	"github.com/ziutek/mymysql/mysql"
+	_ "github.com/ziutek/mymysql/thrsafe"
+	"os"
 )
 
-type Controller interface {
+const (
+	DB_ADDR  = "127.0.0.1:3306"
+	DB_NAME  = "time_tracker"
+	DB_USER  = "root"
+	DB_PASS  = "fawkes"
+	DB_PROTO = "tcp"
+)
+
+func OpenDB() mysql.Conn {
+	db := mysql.New(DB_PROTO, "", DB_ADDR, DB_USER, DB_PASS, DB_NAME)
+
+	fmt.Printf("Connect to %s:%s... ", DB_PROTO, DB_ADDR)
+	checkError(db.Connect())
+	printOK()
+
+	return db
 }
 
-func router (w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func printOK() {
+	fmt.Println("OK")
+}
 
-    var controller Controller
-    
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func checkedResult(rows []mysql.Row, res mysql.Result, err error) ([]mysql.Row,
+	mysql.Result) {
+	checkError(err)
+	return rows, res
+}
+
+type Client struct {
+	ID   int
+	Name string
+}
+
+type ClientsController struct{}
+
+func (cr *ClientsController) Create(cx *goweb.Context) {
+	fmt.Fprintf(cx.ResponseWriter, "Create new resource")
+}
+func (cr *ClientsController) Delete(id string, cx *goweb.Context) {
+	fmt.Fprintf(cx.ResponseWriter, "Delete resource %s", id)
+}
+func (cr *ClientsController) Read(id string, cx *goweb.Context) {
+	fmt.Fprintf(cx.ResponseWriter, "Read resource %s", id)
+}
+func (cr *ClientsController) ReadMany(cx *goweb.Context) {
+	db := OpenDB()
+
+	rows, res := checkedResult(db.Query("select * from clients"))
+
+	id := res.Map("id")
+	name := res.Map("name")
+
+	numRows := len(rows)
+
+	var clients []Client
+	clients = make([]Client, numRows, numRows)
+
+	for ii, row := range rows {
+		clients[ii] = Client{row.Int(id), row.Str(name)}
+	}
+
+    cx.RespondWithData(clients)
+}
+func (cr *ClientsController) Update(id string, cx *goweb.Context) {
+
 }
 
 func main() {
-    http.HandleFunc("/", router)
-    http.ListenAndServe(":8080", nil)
+	// create our resource controller
+	controller := new(ClientsController)
+
+	// make the mapping
+	goweb.MapRest("/clients", controller)
+
+    goweb.ConfigureDefaultFormatters()
+    goweb.ListenAndServe(":8080")
+
+
 }
